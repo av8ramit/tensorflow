19feff93fd22e7e00e71e4c95288fdd0296244a1
Amit Patankar <amitpatankar@google.com>
2018-01-02 10:01:12 -0800

    Testing a python 3.6 package on ubuntu16.04.

--- tensorflow/tools/ci_build/install/install_python3.6_pip_packages.sh
+++ tensorflow/tools/ci_build/install/install_python3.6_pip_packages.sh
@@ -20,8 +20,8 @@
 
 # LINT.IfChange
 
-# fkrull/deadsnakes is for Python3.6
-add-apt-repository -y ppa:fkrull/deadsnakes
+# ppa:jonathonf/python-3.6 is for Python3.6
+add-apt-repository ppa:jonathonf/python-3.6
 
 apt-get update
 apt-get upgrade
@@ -41,15 +41,7 @@ apt-get install libsqlite3-dev
 set -e
 
 # Install Python 3.6 and dev library
-wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tar.xz
-tar xvf Python-3.6.1.tar.xz
-cd Python-3.6.1
-
-./configure
-make altinstall
-pip3.6 -V
-which pip3.6
-ln -s /usr/local/bin/pip3.6 /usr/local/bin/pip3
+apt-get install python3.6
 
 pip3 install --upgrade virtualenv
 
